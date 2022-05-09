0e90387495a88c9a52c45e26c7cf59da4750d462
hugosenari <hugosenari@gmail.com>
2022-05-09 20:38:57 -0300

    chore: re index isaac

--- flake.nix
+++ flake.nix
@@ -7,38 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."isaac-master".dir   = "master";
-  inputs."isaac-master".owner = "nim-nix-pkgs";
-  inputs."isaac-master".ref   = "master";
-  inputs."isaac-master".repo  = "isaac";
-  inputs."isaac-master".type  = "github";
-  inputs."isaac-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."isaac-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."isaac-v0_1_1".dir   = "v0_1_1";
-  inputs."isaac-v0_1_1".owner = "nim-nix-pkgs";
-  inputs."isaac-v0_1_1".ref   = "master";
-  inputs."isaac-v0_1_1".repo  = "isaac";
-  inputs."isaac-v0_1_1".type  = "github";
-  inputs."isaac-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."isaac-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."isaac-v0_1_2".dir   = "v0_1_2";
-  inputs."isaac-v0_1_2".owner = "nim-nix-pkgs";
-  inputs."isaac-v0_1_2".ref   = "master";
-  inputs."isaac-v0_1_2".repo  = "isaac";
-  inputs."isaac-v0_1_2".type  = "github";
-  inputs."isaac-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."isaac-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."isaac-v0_1_3".dir   = "v0_1_3";
-  inputs."isaac-v0_1_3".owner = "nim-nix-pkgs";
-  inputs."isaac-v0_1_3".ref   = "master";
-  inputs."isaac-v0_1_3".repo  = "isaac";
-  inputs."isaac-v0_1_3".type  = "github";
-  inputs."isaac-v0_1_3".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."isaac-v0_1_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
